c0bd18c14ddb6bf548abcbd87fc33d9f2031c0b6
Felipe Cano <felicanoo@gmail.com>
2020-09-02 03:12:38 -0400

    Add log method for get principal

--- java/spring/spring-data-mvc/src/main/java/com/exercise/api/data/controllers/FrontEndController.java
+++ java/spring/spring-data-mvc/src/main/java/com/exercise/api/data/controllers/FrontEndController.java
@@ -1,7 +1,13 @@
 package com.exercise.api.data.controllers;
 
 import com.exercise.api.data.services.TeacherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FrontEndController {
 
     private final TeacherService teacherService;
+    private Logger logger = LoggerFactory.getLogger(FrontEndController.class);
 
     @Autowired
     public FrontEndController(TeacherService teacherService) {
@@ -19,11 +26,25 @@ public class FrontEndController {
     @GetMapping({"/teachers", "/"})
     public String teachers(Model model){
         model.addAttribute("teachers", teacherService.getAll());
+        logPrincipal("/teachers");
         return "teachers";
     }
 
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    private void logPrincipal(String resource){
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        Object principal = authentication.getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        logger.info("Principal '" + username + "' is accessing to '" + resource + "'");
     }
 }
